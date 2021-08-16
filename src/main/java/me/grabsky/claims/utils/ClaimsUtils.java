b8f7b9e00fe4dcbbe67825eed0ce028bb968202b
Grabsky <grabskypl@gmail.com>
2021-08-16 19:49:12 +0200

    minor code style improvements

--- src/main/java/me/grabsky/claims/utils/ClaimsUtils.java
+++ src/main/java/me/grabsky/claims/utils/ClaimsUtils.java
@@ -37,25 +37,21 @@ public class ClaimsUtils {
     public static List<Object> getFlagOptions(Flag<?> flagType) {
         if (flagType instanceof StateFlag) return Arrays.asList(StateFlag.State.ALLOW, StateFlag.State.DENY);
         // Comparing instances is useless in some cases - let's check the name instead
-        switch (flagType.getName()) {
-            case "weather-lock":
-                return Arrays.asList(null, WeatherTypes.CLEAR, WeatherTypes.RAIN, WeatherTypes.THUNDER_STORM);
-            case "time-lock":
-                return Arrays.asList(null, "0", "6000", "12000", "18000");
-        }
-        return null;
+        return switch (flagType.getName()) {
+            case "weather-lock" -> Arrays.asList(null, WeatherTypes.CLEAR, WeatherTypes.RAIN, WeatherTypes.THUNDER_STORM);
+            case "time-lock" -> Arrays.asList(null, "0", "6000", "12000", "18000");
+            default -> null;
+        };
     }
 
     // Returns list of options (formatted flag values) displayed in GUI
     public static List<String> getFormattedFlagOptions(Flag<?> flagType) {
         if (flagType instanceof StateFlag) return Arrays.asList("Włączone", "Wyłączone");
         // Comparing instances is pointless in some cases - let's check the name instead
-        switch (flagType.getName()) {
-            case "weather-lock":
-                return Arrays.asList("Wyłączone", "Słonecznie", "Deszcz", "Burza");
-            case "time-lock":
-                return Arrays.asList("Wyłączone", "Rano (6:00)", "Południe (12:00)", "Wieczór (18:00)", "Północ (00:00)");
-        }
-        return null;
+        return switch (flagType.getName()) {
+            case "weather-lock" -> Arrays.asList("Wyłączone", "Słonecznie", "Deszcz", "Burza");
+            case "time-lock" -> Arrays.asList("Wyłączone", "Rano (6:00)", "Południe (12:00)", "Wieczór (18:00)", "Północ (00:00)");
+            default -> null;
+        };
     }
 }
