84ec96ffb0083ce165e0a01f42f33e1184b444db
Michał Czopek <44530932+Grabsky@users.noreply.github.com>
2024-12-04 14:21:37 +0100

    relicense under GPLv3

--- src/main/java/cloud/grabsky/claims/util/Extensions.java
+++ src/main/java/cloud/grabsky/claims/util/Extensions.java
@@ -1,3 +1,17 @@
+/*
+ * Claims (https://github.com/Grabsky/Claims)
+ *
+ * Copyright (C) 2024  Grabsky <michal.czopek.foss@proton.me>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License v3 as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License v3 for more details.
+ */
 package cloud.grabsky.claims.util;
 
 import net.kyori.adventure.audience.Audience;
